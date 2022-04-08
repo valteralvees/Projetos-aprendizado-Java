c33ca7c9276d108572c5a09806bc3f9f6fc747d0
Valter Alves <valteralvees@outlook.com>
2022-04-08 15:24:51 -0300

    Update em Resolucao de tela

--- ResolucaoDeTela/src/main/java/com/mycompany/resolucaodetela/ResolucaoDeTela.java
+++ ResolucaoDeTela/src/main/java/com/mycompany/resolucaodetela/ResolucaoDeTela.java
@@ -1,17 +1,17 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
- */
-
 package com.mycompany.resolucaodetela;
 
-/**
- *
- * @author morga
- */
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
 public class ResolucaoDeTela {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Saberemos agora a sua resolução de tela. ");
+        Dimension tela = Toolkit.getDefaultToolkit().getScreenSize();
+        int alt = (int) tela.getHeight();
+        int lar = (int) tela.getWidth();
+        System.out.println("Sua tela tem a resolução de " + lar + "x" + alt);
+        
+        
     }
 }
